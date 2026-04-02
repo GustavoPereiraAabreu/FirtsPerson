5b9d85150732598244ba5bbe8882bd85d39fefd0
GUSTAVO PEREIRA ABREU <gustavop.4breu@gmail.com>
2026-04-02 14:38:21 -0300

    Ajustes

--- Assets/GU/Scripts/EnemyStatus.cs
+++ Assets/GU/Scripts/EnemyStatus.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class EnemyStatus : MonoBehaviour, IShootable
 {
+    [SerializeField] float _health = 2;
+    private float _currentHealth;
+
     public void Hitted(float damage)
     {
+        _currentHealth -= damage;
+        if (_currentHealth > 0)
+            return;
+            
         Destroy(gameObject);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _currentHealth = _health;
     }
 
     // Update is called once per frame
--- Assets/GU/Scripts/Gun.cs
+++ Assets/GU/Scripts/Gun.cs
@@ -6,7 +6,6 @@ public class Gun : MonoBehaviour
     private Transform _camera;
 
 
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,15 +15,18 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Verifica se player atirou
-        if (Input.GetButtonDown("Fire1"))
+         //Verifica se o player atirou
+        if (!Input.GetButtonDown("Fire1"))
             return;
         //Verifica se o player acertou algo na layer Shootable
         if (!Physics.Raycast(_camera.position, _camera.forward, out RaycastHit target))
             return;
-        //Verifica se o objeto implementa IShootable
+        //Verifica se o objeto acertado implementa IShootable
         if (!target.collider.TryGetComponent(out IShootable shootable))
             return;
+
+        //Aciona o método do contrato IShootable
+        shootable.Hitted(1);
 
         //Aciona o método do contrato IShootable
         shootable.Hitted(1);
